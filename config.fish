06f19204893f530bf33ce28e8ab300bab62cf617
João Gustavo <joao.gustavo@sisqualis.com.br>
2016-01-20 08:35:33 -0200

    Ajusta path android sdk

--- config.fish
+++ config.fish
@@ -1,17 +1,22 @@
 # Configs
-set ANDROID_HOME /Users/joao/Downloads/android-sdk-macosx
-set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
-set PATH $ANDROID_HOME/platform-tools $PATH
-set PATH /usr/local/sbin $PATH
-set PATH $ANDROID_HOME/tools $PATH
-set PATH $ANDROID_HOME/build-tools/21.1.2 $PATH
+
+# Incluindo SDK do android
+set PATH ~/work/.android-sdk/platform-tools $PATH
+set PATH ~/work/.android-sdk/tools $PATH
+set PATH ~/work/.android-sdk/build-tools/23.0.2 $PATH
+set ANDROID_HOME ~/work/.android-sdk/
+set ANDROID_TOOLS ~/work/.android-sdk/
+set ANDROID_PLATFORM_TOOLS ~/work/.android-sdk/
+
 set ANT_HOME /usr/local/bin/ant
+set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
 set PATH $ANT_HOME/bin $PATH
 set PATH /usr/X11/bin $PATH
+set PATH /usr/local/sbin $PATH
 set --export NODE_PATH /usr/local/lib/node_modules
 
 # Adicionando workon - Deve ser após modificar o path.
-. ~/.config/fish/fishenv.fish
+. ~/work/tests/FishEnv/fishenv.fish
 
 
 # Workon JF
