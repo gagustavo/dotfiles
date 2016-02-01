3beab63d76cd1bc856c0c09baf50532d235b250c
João Gustavo <joao.gustavo@sisqualis.com.br>
2016-02-01 09:25:31 -0200

    Atualiza confis mac

--- config.fish
+++ config.fish
@@ -9,20 +9,14 @@ set ANDROID_TOOLS ~/work/.android-sdk/
 set ANDROID_PLATFORM_TOOLS ~/work/.android-sdk/
 
 set ANT_HOME /usr/local/bin/ant
-set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
+set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_71.jdk/Contents/Home
 set PATH $ANT_HOME/bin $PATH
 set PATH /usr/X11/bin $PATH
 set PATH /usr/local/sbin $PATH
 set --export NODE_PATH /usr/local/lib/node_modules
 
 # Adicionando workon - Deve ser após modificar o path.
-. ~/work/tests/FishEnv/fishenv.fish
-
-
-# Workon JF
-function jfstart
-    workon jf
-end
+. ~/work/FishEnv/fishenv.fish
 
 # Por fim abre o tmux
 tmux
