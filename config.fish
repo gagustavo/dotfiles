2ae99103a112db1f60871e1df1e8a905f7194f56
João Gustavo <gagu2030@gmail.com>
2018-09-10 20:59:50 -0300

    Habilita GPG
    * Corrige tabulação
    * Corrige includes no PATH

--- config.fish
+++ config.fish
@@ -2,11 +2,13 @@
 #
 
 set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set PATH /usr/local/opt/dirmngr/bin $PATH
-set PATH /usr/local/opt/gpg-agent/bin $PATH
-set PATH /usr/local/opt/icu4c/bin $PATH
-set PATH /usr/local/opt/icu4c/sbin $PATH
-set PATH /usr/local/opt/libpq/bin $PATH
+set PATH /usr/local/opt/mysql-client/bin $PATH
+
+# set PATH /usr/local/opt/dirmngr/bin $PATH
+# set PATH /usr/local/opt/gpg-agent/bin $PATH
+# set PATH /usr/local/opt/icu4c/bin $PATH
+# set PATH /usr/local/opt/icu4c/sbin $PATH
+# set PATH /usr/local/opt/libpq/bin $PATH
 
 # Coreutils
 set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
@@ -15,10 +17,10 @@ set PATH /usr/local/sbin $PATH
 set --export NODE_PATH /usr/local/lib/node_modules
 
 # ANDROID
-set ANDROID_NDK_HOME /usr/local/share/android-ndk
-set ANDROID_SDK_ROOT /usr/local/share/android-sdk
-set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
-set PATH $ANDROID_SDK_ROOT/tools $PATH
+# set ANDROID_NDK_HOME /usr/local/share/android-ndk
+# set ANDROID_SDK_ROOT /usr/local/share/android-sdk
+# set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
+# set PATH $ANDROID_SDK_ROOT/tools $PATH
 
 
 set LDFLAGS "-L/usr/local/opt/libpq/lib"
